ae42e6ab09d241c2aecf84685766da8c029d831f
fatkhur99 <47076641+fatkhur99@users.noreply.github.com>
2019-03-06 03:25:42 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,24 @@
+<?php
+// configurasi //
+
+
+// user_name boleh kosong //
+$user_name="";
+
+//user_gaid wajib isi
+$user_gaid= "4ad38dfd-9772-450c-abbf-4552b60be6db";
+//user_token wajib di isi //
+$user_token="AbzhSxWBf5F6_65U";
+
+//user_id wajib di isi //
+$user_id="Facebook_107108033759026";
+
+// invite_code wajib di isi //
+$invite_code="FUPhDGH7C";
+
+// Bearer wajib di isi //
+$Bearer="eyxxxxxxxxxxxxxxxxxx";
+
+
+
+?>
